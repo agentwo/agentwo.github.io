fa11b0923c4ded0722ae26dad1a9658c0988a1bf
Agent Wo <sedatkoc_public@protonmail.com>
2022-08-21 23:17:53 +0300

    First commit

---
+++ precache-manifest.443b9bb6c117613c6b4a38e3c698a218.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f3e5b865b52b0b5d9a6a92ec173403e6",
+    "url": "/index.html"
+  },
+  {
+    "revision": "133c2a1b43b27d10f4f8",
+    "url": "/static/css/2.d64471e2.chunk.css"
+  },
+  {
+    "revision": "19a57622d18bc524f9d5",
+    "url": "/static/css/main.db76a3b0.chunk.css"
+  },
+  {
+    "revision": "133c2a1b43b27d10f4f8",
+    "url": "/static/js/2.e40a2ef3.chunk.js"
+  },
+  {
+    "revision": "7c4b6536cab2d1970758ff058c5fbbf8",
+    "url": "/static/js/2.e40a2ef3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "19a57622d18bc524f9d5",
+    "url": "/static/js/main.28c85d06.chunk.js"
+  },
+  {
+    "revision": "5d75b077a33ee2cd7211",
+    "url": "/static/js/runtime-main.647fda42.js"
+  }
+]);
